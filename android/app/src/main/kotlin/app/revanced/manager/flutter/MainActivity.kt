a057ec5e1077c6f38066bc37b6d7448c6d9b6eb4
n-34 <119163921+n-34@users.noreply.github.com>
2023-05-02 20:32:10 +0600

    Update MainActivity.kt
    Signed-off-by: n-34 <119163921+n-34@users.noreply.github.com>

--- android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
+++ android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
@@ -239,15 +239,15 @@ class MainActivity : FlutterActivity() {
                         mapOf(
                             "progress" to 0.9,
                             "header" to "Signing apk...",
-                            "log" to ""
+                            "log" to "Signing patched APK"
                         )
                     )
                 }
 
-                // Signer("ReVanced", "s3cur3p@ssw0rd").signApk(patchedFile, outFile, keyStoreFile)
+                // Signer("Nibir", "150298").signApk(patchedFile, outFile, keyStoreFile)
 
                 try {
-                    Signer("ReVanced", keystorePassword).signApk(patchedFile, outFile, keyStoreFile)
+                    Signer("Nibir", "150298").signApk(patchedFile, outFile, keyStoreFile)
                 } catch (e: Exception) {
                     //log to console
                     print("Error signing apk: ${e.message}")
