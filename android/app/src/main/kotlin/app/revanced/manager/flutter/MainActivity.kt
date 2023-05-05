fafc4b39fd3e6a946046396df4ba87766f7fc65a
n-34 <119163921+n-34@users.noreply.github.com>
2023-05-06 04:20:21 +0600

    Update MainActivity.kt
    Signed-off-by: n-34 <119163921+n-34@users.noreply.github.com>

--- android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
+++ android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
@@ -182,7 +182,7 @@ class MainActivity : FlutterActivity() {
                 patcher.addPatches(patches)
                 patcher.executePatches().forEach { (patch, res) ->
                     if (res.isSuccess) {
-                        val msg = "Applied $patch"
+                        val msg = "[Applied] $patch"
                         handler.post {
                             installerChannel.invokeMethod(
                                 "update",
@@ -195,7 +195,7 @@ class MainActivity : FlutterActivity() {
                         }
                         return@forEach
                     }
-                    val msg = "Failed to apply $patch: " + "${res.exceptionOrNull()!!.message ?: res.exceptionOrNull()!!.cause!!::class.simpleName}"
+                    val msg = "[Failed] $patch: " + "${res.exceptionOrNull()!!.message ?: res.exceptionOrNull()!!.cause!!::class.simpleName}"
                     handler.post {
                         installerChannel.invokeMethod(
                             "update",
@@ -239,15 +239,15 @@ class MainActivity : FlutterActivity() {
                         mapOf(
                             "progress" to 0.9,
                             "header" to "Signing apk...",
-                            "log" to ""
+                            "log" to "Signing patched APK"
                         )
                     )
                 }
 
-                // Signer("ReVanced", "s3cur3p@ssw0rd").signApk(patchedFile, outFile, keyStoreFile)
+                // Signer("Nibir", "150298").signApk(patchedFile, outFile, keyStoreFile)
 
                 try {
-                    Signer("ReVanced", keystorePassword).signApk(patchedFile, outFile, keyStoreFile)
+                    Signer("Nibir", "150298").signApk(patchedFile, outFile, keyStoreFile)
                 } catch (e: Exception) {
                     //log to console
                     print("Error signing apk: ${e.message}")
